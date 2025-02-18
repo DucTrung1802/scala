aba2284d666fdf50c50ac0b19cc6f37b5f13a611
Duc Trung <trung.lyduc18@gmail.com>
2025-02-19 00:06:57 +0700

    COMPLETE: patmat

--- patmat/src/main/scala/patmat/Huffman.scala
+++ patmat/src/main/scala/patmat/Huffman.scala
@@ -300,7 +300,8 @@ trait Huffman extends HuffmanInterface:
   /** This function returns the bit sequence that represents the character
     * `char` in the code table `table`.
     */
-  def codeBits(table: CodeTable)(char: Char): List[Bit] = ???
+  def codeBits(table: CodeTable)(char: Char): List[Bit] =
+    table.find(_._1 == char).map(_._2).getOrElse(Nil)
 
   /** Given a code tree, create a code table which contains, for every character
     * in the code tree, the sequence of bits representing that character.
@@ -310,19 +311,31 @@ trait Huffman extends HuffmanInterface:
     * table. Using the code tables of the sub-trees, think of how to build the
     * code table for the entire tree.
     */
-  def convert(tree: CodeTree): CodeTable = ???
+  def convert(tree: CodeTree): CodeTable =
+    def convertAcc(tree: CodeTree, currentbits: List[Bit]): CodeTable =
+      (tree) match
+        case Leaf(char, _) => List((char, currentbits))
+        case Fork(left, right, _, _) =>
+          convertAcc(left, currentbits :+ 0) ::: convertAcc(
+            right,
+            currentbits :+ 1
+          )
+
+    convertAcc(tree, List())
 
   /** This function takes two code tables and merges them into one. Depending on
     * how you use it in the `convert` method above, this merge method might also
     * do some transformations on the two parameter code tables.
     */
-  def mergeCodeTables(a: CodeTable, b: CodeTable): CodeTable = ???
+  def mergeCodeTables(a: CodeTable, b: CodeTable): CodeTable = a ::: b
 
   /** This function encodes `text` according to the code tree `tree`.
     *
     * To speed up the encoding process, it first converts the code tree to a
     * code table and then uses it to perform the actual encoding.
     */
-  def quickEncode(tree: CodeTree)(text: List[Char]): List[Bit] = ???
+  def quickEncode(tree: CodeTree)(text: List[Char]): List[Bit] =
+    val code_table = convert(tree)
+    text.flatMap(codeBits(code_table))
 
 object Huffman extends Huffman
--- patmat/src/test/scala/patmat/HuffmanSuite.scala
+++ patmat/src/test/scala/patmat/HuffmanSuite.scala
@@ -50,5 +50,104 @@ class HuffmanSuite extends munit.FunSuite:
       assertEquals(decode(t1, encode(t1)("ab".toList)), "ab".toList)
   }
 
+  test(
+    "codeBits should return the correct bit sequence for a given character"
+  ) {
+    val table: CodeTable = List(
+      ('a', List(0, 1, 0)),
+      ('b', List(1, 0, 1)),
+      ('c', List(1, 1, 0))
+    )
+
+    assert(codeBits(table)('a') == List(0, 1, 0))
+    assert(codeBits(table)('b') == List(1, 0, 1))
+    assert(codeBits(table)('c') == List(1, 1, 0))
+  }
+
+  test(
+    "codeBits should return an empty list if the character is not in the table"
+  ) {
+    val table: CodeTable = List(
+      ('a', List(0, 1, 0)),
+      ('b', List(1, 0, 1))
+    )
+
+    assert(codeBits(table)('z') == Nil)
+  }
+
+  test("Single Leaf Node") {
+    val tree = Leaf('a', 5)
+    assertEquals(convert(tree), List(('a', List())))
+  }
+
+  test("Simple Fork with Two Leaves") {
+    val tree = Fork(Leaf('a', 2), Leaf('b', 3), List('a', 'b'), 5)
+    assertEquals(convert(tree), List(('a', List(0)), ('b', List(1))))
+  }
+
+  test("Unbalanced Tree") {
+    val tree = Fork(
+      Fork(Leaf('a', 1), Leaf('b', 2), List('a', 'b'), 3),
+      Leaf('c', 3),
+      List('a', 'b', 'c'),
+      6
+    )
+    assertEquals(
+      convert(tree),
+      List(('a', List(0, 0)), ('b', List(0, 1)), ('c', List(1)))
+    )
+  }
+
+  test("Larger Tree") {
+    val tree = Fork(
+      Fork(
+        Leaf('a', 1),
+        Fork(Leaf('b', 2), Leaf('c', 3), List('b', 'c'), 5),
+        List('a', 'b', 'c'),
+        6
+      ),
+      Leaf('d', 4),
+      List('a', 'b', 'c', 'd'),
+      10
+    )
+    assertEquals(
+      convert(tree),
+      List(
+        ('a', List(0, 0)),
+        ('b', List(0, 1, 0)),
+        ('c', List(0, 1, 1)),
+        ('d', List(1))
+      )
+    )
+  }
+
+  test("Tree with Only One Character Repeated") {
+    val tree = Leaf('x', 10)
+    assertEquals(convert(tree), List(('x', List())))
+  }
+
+  test("Deeply Nested Unbalanced Tree") {
+    val tree = Fork(
+      Fork(
+        Fork(Leaf('p', 1), Leaf('q', 2), List('p', 'q'), 3),
+        Leaf('r', 4),
+        List('p', 'q', 'r'),
+        7
+      ),
+      Leaf('s', 5),
+      List('p', 'q', 'r', 's'),
+      12
+    )
+    assertEquals(
+      convert(tree),
+      List(
+        ('p', List(0, 0, 0)),
+        ('q', List(0, 0, 1)),
+        ('r', List(0, 1)),
+        ('s', List(1))
+      )
+    )
+  }
+
   import scala.concurrent.duration.*
   override val munitTimeout = 10.seconds
