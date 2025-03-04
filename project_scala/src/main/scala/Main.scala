4d45217295c26f1eb125f683da047b9ed7adbb6a
DucTrung1802 <trung.lyduc18@gmail.com>
2025-03-05 04:58:41 +0700

    UPDATE: need to download model

--- project_scala/src/main/scala/Main.scala
+++ project_scala/src/main/scala/Main.scala
@@ -5,40 +5,26 @@ import com.johnsnowlabs.nlp.annotators.seq2seq.T5Transformer
 
 object App {
   def main(args: Array[String]): Unit = {
-    // Create Spark Session
     val spark = SparkSession
       .builder()
-      .appName("Spark NLP - T5 Transformer")
-      .master("local[*]") // Use local mode
+      .appName("Spark NLP Example")
+      .master("local[*]")
       .getOrCreate()
 
     import spark.implicits._
 
-    // Define Document Assembler
     val documentAssembler = new DocumentAssembler()
       .setInputCol("text")
       .setOutputCol("document")
 
-    // Load Pretrained T5 Transformer
     val t5 = T5Transformer
       .pretrained("t5_large_arxiv_abstract_title", "en")
-      .setInputCols(Array("document"))
+      .setInputCols(Array("documents"))
       .setOutputCol("output")
 
-    // Define Pipeline
     val pipeline = new Pipeline().setStages(Array(documentAssembler, t5))
-
-    // Sample Data
-    val data = Seq("I love Spark NLP").toDS.toDF("text")
-
-    // Fit and Transform Data
+    val data = Seq("I love spark-nlp").toDS.toDF("text")
     val pipelineModel = pipeline.fit(data)
     val pipelineDF = pipelineModel.transform(data)
-
-    // Show Results
-    pipelineDF.select("text", "output.result").show(truncate = false)
-
-    // Stop Spark Session
-    spark.stop()
   }
 }
