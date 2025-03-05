69f25d60283540376306a17da620f13136d7aa52
DucTrung1802 <trung.lyduc18@gmail.com>
2025-03-06 02:03:04 +0700

    UPDATE: code python

--- project/project_python/t5_large_arxiv_abstract_title.py
+++ project/project_python/t5_large_arxiv_abstract_title.py
@@ -10,25 +10,14 @@ T5_LARGE_ARXIV_ABSTRACT_TITLE_NAME = "t5_large_arxiv_abstract_title"
 # Initialize Spark session
 spark = (
     SparkSession.builder.appName("SparkNLP")
-    .config("spark.driver.memory", "16G")
-    .config("spark.kryoserializer.buffer.max", "1000M")
-    .config("spark.jars.packages", "com.johnsnowlabs.nlp:spark-nlp_2.12:5.1.3")
+    .config("spark.sql.execution.arrow.pyspark.enabled", "false")
+    .config("spark.jars.packages", "com.johnsnowlabs.nlp:spark-nlp_2.12:5.5.3")
     .getOrCreate()
 )
 
 documentAssembler = DocumentAssembler().setInputCol("text").setOutputCol("document")
 
-t5 = None
-try:
-    with open(f"{T5_LARGE_ARXIV_ABSTRACT_TITLE_NAME}.pkl", "rb") as file:
-        t5 = pickle.load(file)
-except:
-    print(
-        f"Failed to load T5 model. Downloading {T5_LARGE_ARXIV_ABSTRACT_TITLE_NAME} model."
-    )
-    t5 = T5Transformer.pretrained("t5_large_arxiv_abstract_title", "en")
-    with open(f"{T5_LARGE_ARXIV_ABSTRACT_TITLE_NAME}.pkl", "wb") as file:
-        pickle.dump(t5, file)
+t5 = T5Transformer.load("../t5_large_arxiv_abstract_title_en_5.4.2_3.0")
 
 t5.setInputCols(["document"]).setOutputCol("output")
 
@@ -36,3 +25,4 @@ pipeline = Pipeline().setStages([documentAssembler, t5])
 data = spark.createDataFrame([["I love spark-nlp"]]).toDF("text")
 pipelineModel = pipeline.fit(data)
 pipelineDF = pipelineModel.transform(data)
+pipelineDF.select("output.result").show(truncate=False)
